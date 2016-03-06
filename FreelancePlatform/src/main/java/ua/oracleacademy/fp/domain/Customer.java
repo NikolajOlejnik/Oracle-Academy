aab53eea75b51f585cd240c0559b679f26bf1c85
NikolajOlejnik <olejnik.dev@gmail.com>
2016-03-06 09:05:38 +0200

    suppress warnings

--- FreelancePlatform/src/main/java/ua/oracleacademy/fp/domain/Customer.java
+++ FreelancePlatform/src/main/java/ua/oracleacademy/fp/domain/Customer.java
@@ -1,23 +1,23 @@
-package ua.oracleacademy.fp.domain;
-
-import javax.persistence.*;
-import java.util.List;
-
-@Entity
-public class Customer extends User {
-
-    @Column
-    @OneToMany(fetch = FetchType.LAZY)
-    private List<Task> orderHistory;
-
-    public Customer() {
-    }
-
-    public List<Task> getOrderHistory() {
-        return orderHistory;
-    }
-
-    public void setOrderHistory(List<Task> orderHistory) {
-        this.orderHistory = orderHistory;
-    }
-}
+//package ua.oracleacademy.fp.domain;
+//
+//import javax.persistence.*;
+//import java.util.List;
+//
+//@Entity
+//public class Customer extends User {
+//
+//    @Column
+//    @OneToMany(fetch = FetchType.LAZY)
+//    private List<Task> orderHistory;
+//
+//    public Customer() {
+//    }
+//
+//    public List<Task> getOrderHistory() {
+//        return orderHistory;
+//    }
+//
+//    public void setOrderHistory(List<Task> orderHistory) {
+//        this.orderHistory = orderHistory;
+//    }
+//}
--- FreelancePlatform/src/main/java/ua/oracleacademy/fp/domain/Task.java
+++ FreelancePlatform/src/main/java/ua/oracleacademy/fp/domain/Task.java
@@ -1,93 +1,93 @@
-package ua.oracleacademy.fp.domain;
-
-import javax.persistence.*;
-import java.util.Date;
-
-@Entity
-@Table(name = "TASK")
-public class Task {
-
-	@Id
-	@Column(name = "TASK_ID")
-	@GeneratedValue
-	private Integer id;
-
-	@Column(name = "DESCRIPTION")
-	private String description;
-
-	@Column(name = "CREATED")
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date date;
-
-	@Column(name = "PRICE")
-	private Double price;
-
-	private Customer customer;
-
-	private Freelancer freelancer;
-
-	@Column(name = "OWNER_ID")
-	private long customerId;
-
-	@Column(name = "FREELANCER_ID")
-	private long freelancerId;
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public Date getDate() {
-		return date;
-	}
-
-	public void setDate(Date date) {
-		this.date = date;
-	}
-
-	public Double getPrice() {
-		return price;
-	}
-
-	public void setPrice(Double price) {
-		this.price = price;
-	}
-
-	public Customer getCustomer() {
-		return customer;
-	}
-
-	public void setCustomer(Customer customer) {
-		this.customer = customer;
-		this.customerId = customer.getId();
-	}
-
-	public Freelancer getFreelancer() {
-		return freelancer;
-	}
-
-	public void setFreelancer(Freelancer freelancer) {
-		this.freelancer = freelancer;
-		this.freelancerId = freelancer.getId();
-	}
-
-	public long getCustomerId() {
-		return customerId;
-	}
-
-
-	public long getFreelancerId() {
-		return freelancerId;
-	}
-}
+//package ua.oracleacademy.fp.domain;
+//
+//import javax.persistence.*;
+//import java.util.Date;
+//
+//@Entity
+//@Table(name = "TASK")
+//public class Task {
+//
+//	@Id
+//	@Column(name = "TASK_ID")
+//	@GeneratedValue
+//	private Integer id;
+//
+//	@Column(name = "DESCRIPTION")
+//	private String description;
+//
+//	@Column(name = "CREATED")
+//	@Temporal(TemporalType.TIMESTAMP)
+//	private Date date;
+//
+//	@Column(name = "PRICE")
+//	private Double price;
+//
+//	private Customer customer;
+//
+//	private Freelancer freelancer;
+//
+//	@Column(name = "OWNER_ID")
+//	private long customerId;
+//
+//	@Column(name = "FREELANCER_ID")
+//	private long freelancerId;
+//
+//	public Integer getId() {
+//		return id;
+//	}
+//
+//	public void setId(Integer id) {
+//		this.id = id;
+//	}
+//
+//	public String getDescription() {
+//		return description;
+//	}
+//
+//	public void setDescription(String description) {
+//		this.description = description;
+//	}
+//
+//	public Date getDate() {
+//		return date;
+//	}
+//
+//	public void setDate(Date date) {
+//		this.date = date;
+//	}
+//
+//	public Double getPrice() {
+//		return price;
+//	}
+//
+//	public void setPrice(Double price) {
+//		this.price = price;
+//	}
+//
+//	public Customer getCustomer() {
+//		return customer;
+//	}
+//
+//	public void setCustomer(Customer customer) {
+//		this.customer = customer;
+//		this.customerId = customer.getId();
+//	}
+//
+//	public Freelancer getFreelancer() {
+//		return freelancer;
+//	}
+//
+//	public void setFreelancer(Freelancer freelancer) {
+//		this.freelancer = freelancer;
+//		this.freelancerId = freelancer.getId();
+//	}
+//
+//	public long getCustomerId() {
+//		return customerId;
+//	}
+//
+//
+//	public long getFreelancerId() {
+//		return freelancerId;
+//	}
+//}
