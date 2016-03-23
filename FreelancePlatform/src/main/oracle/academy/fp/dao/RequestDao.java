66ba32601c6f87fb35cf546b2ee01e919820e818
NikolajOlejnik <olejnik.dev@gmail.com>
2016-03-23 17:06:25 +0200

    clean up

--- FreelancePlatform/src/main/oracle/academy/fp/dao/RequestDao.java
+++ FreelancePlatform/src/main/oracle/academy/fp/dao/RequestDao.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface RequestDao {
     Request create (Request request);
-    List<Request> getAllRequestByTaskId(Long taskId);
+    List getAllRequestByTaskId(Long taskId);
     Request getRequestById(Long requestId);
     Boolean update(Request request);
     List<Request> getAllUserRequests(Long id);
